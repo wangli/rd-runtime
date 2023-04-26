b5aa66453c46732101c89f891712fb4a62088d87
wanglip8 <lovingyou@qq.com>
2023-04-26 14:46:17 +0800

    up

--- src/display/coms/SpriteObject.js
+++ src/display/coms/SpriteObject.js
@@ -1,4 +1,4 @@
-import { h } from 'vue'
+import { h, provide, reactive } from 'vue'
 import { getAppGlobal } from '@/utils'
 import createSprite from '../createSprite'
 import createProps from '../createProps'
@@ -14,9 +14,15 @@ export default {
       const AppSetup = getAppGlobal('AppSetup')
       const data = getAppGlobal('data')
       const component = getAppGlobal('component')
+      const rect = reactive({ x: 0, y: 0, width: 0, height: 0 })
+      provide('rect', rect)
       lifecycleHook(vnode, props, context)
       return () => {
          const myData = data.getElement(props.id)
+         rect.x = myData.x
+         rect.y = myData.y
+         rect.width = myData.width
+         rect.height = myData.height
          let myProps = createProps(myData, { id: props.id, myApp: { AppSetup, data, component } })
          let slot = createSprite({ name: myData.name, props: myData.id })
          vnode.value = h('div', myProps, slot)
